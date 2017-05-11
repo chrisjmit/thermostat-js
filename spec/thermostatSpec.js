583ff0d0e5abadfe62baaf227880293958446dfa
Robert Jones <robert.g.jones@gmail.com>
2017-05-11 12:22:54 +0100

    Thermostat has up function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,8 +1,14 @@
-describe("thermostat exists", function(){
-  thermostat = new Thermostat();
+describe("A Thermostat", function(){
 
-  it('exists as an object', function() {
+  var thermostat = new Thermostat();
+
+  it('has a default temperature', function() {
     expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('can be turned up', function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,8 @@
 function Thermostat() {
-
     this.temperature = 20;
-
 }
+
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1 ;
+};
