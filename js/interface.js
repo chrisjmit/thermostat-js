86d56f1843446a16f6b42fc684fa95ff98b09a39
Chris Mitchell <chrisjmit@gmail.com>
2017-05-12 11:51:23 +0100

    text display added

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   alert("The Bo$$man welcomes you");
+  //
+  // $( "powersave-off" ).click(function( ) {
+  //       alert( "Powersaving is now off!" );
+  //   });
+  //
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
 });
